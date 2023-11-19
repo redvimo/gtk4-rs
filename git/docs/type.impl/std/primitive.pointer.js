0f6e90487163c325134592cc80a6dcdc126e9a44
sdroege <sdroege@users.noreply.github.com>
2023-11-19 11:31:08 +0000

    deploy: 1beb9eb7c3150ae8aa4a77881dfe26a4006dc22d

---
+++ git/docs/type.impl/std/primitive.pointer.js
@@ -0,0 +1,9 @@
+(function() {var type_impls = {
+"gdk4":[],
+"gdk4_sys":[],
+"gdk4_wayland_sys":[],
+"gdk4_win32_sys":[],
+"gdk4_x11_sys":[],
+"gsk4_sys":[],
+"gtk4_sys":[]
+};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
